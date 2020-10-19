713b92eafa395cdd531ef4df47e9f5875044eaca
DiegoRamires <goncalves.ramires@gmail.com>
2020-10-19 18:27:54 -0300

    feature: add landing page basic markup and style

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,49 @@
 import React from 'react'
 
+import { Form, ResultSearch } from './styles'
+
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>
+  return (
+    <>
+      <Form>
+        <input placeholder="Digite o planeta que gostaria de buscar na API" />
+        <button>Pesquisar</button>
+      </Form>
+
+      <ResultSearch>
+        <a href='test'>
+          <p>
+            <strong>Nome:</strong>
+            <span>tattoine</span>
+          </p>
+          <p>
+            <strong>Período de rotação:</strong>
+            <span>23 horas</span>
+          </p>
+          <p>
+            <strong>Período de orbita:</strong>
+            <span>341 dias</span>
+          </p>
+          <p>
+            <strong>Clima:</strong>
+            <span>murky</span>
+          </p>
+          <p>
+            <strong>terreno:</strong>
+            <span>swamp, jungles</span>
+          </p>
+          <p>
+            <strong>agua superfície:</strong>
+            <span>8</span>
+          </p>
+          <p>
+            <strong>população:</strong>
+            <span>desconhecida</span>
+          </p>
+        </a>
+      </ResultSearch>
+    </>
+  )
 }
 
 export default Dashboard
